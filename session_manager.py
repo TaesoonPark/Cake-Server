9b5f32f4cc73bd7bfaee8aa5a7393d318f4d661b
parkts <parkts@localhost.localdomain>
2020-11-10 10:56:46 +0900

    세션 매니저 쓰레드 락 추가
    - 쓰레드 락 추가

--- session_manager.py
+++ session_manager.py
@@ -1,46 +1,68 @@
+import threading
 import json
 
 client_sockets = dict();
+client_sockets_lock = threading.Lock();
 
 def addSession(session_id, client_socket):
   global client_sockets;
-  
+
+  client_sockets_lock.acquire();
+
   if session_id in client_sockets:
     print("duplicate session", session_id);
     return False;
 
   client_sockets[session_id] = client_socket;
 
+  client_sockets_lock.release();
+
   return True;
+
 
 def getSession(session_id):
   global client_sockets;
+
+  client_sockets_lock.acquire();
 
   if session_id not in client_sockets:
     print("not exist session", session_id);
     return None;
 
+  client_sockets_lock.release();
+
   return client_sockets[session_id];
+
 
 def removeSession(session_id):
   global client_sockets;
+
+  client_sockets_lock.acquire();
 
   if session_id in client_sockets:
     print("socket closed", session_id);
     client_sockets[session_id].close();
     del client_sockets[session_id];
 
+  client_sockets_lock.release();
+
+
 def sendMessage(session_id, message):
   global client_sockets;
+
+  msg = json.dumps(message);
+  print("send message= ", msg);
+  bytes = msg.encode();
+  msg_len = len(bytes);
+
+  client_sockets_lock.acquire();
 
   if session_id not in client_sockets:
     print("cannot find session", session_id);
     return;
 
   client_socket = client_sockets[session_id];
-  msg = json.dumps(message);
-  print("send message= ", msg);
-  bytes = msg.encode();
-  msg_len = len(bytes);
   client_socket.sendall(msg_len.to_bytes(4, byteorder="little"));
   client_socket.sendall(bytes);
+
+  client_sockets_lock.release();
